f357772f71d2a85bcd51a05b89dcde67475a388d
Klemen Pavlovič <klemenpavlovic@gmail.com>
2023-07-24 10:31:38 +0200

    Add rainbow kit to the app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+// Added due to legacy issues with a dependancy inside of RainbowKit, this has to be added to prevent crashes
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import "@/styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "CryptoDevs DAO",
+  projectId: process.env.WALLET_CONNECT_PROJECT_ID,
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
-
-export default MyApp
